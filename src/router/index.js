f26b2e5efa33f617ed5013e5468d55218c3284ad
wiryadev <abrarwiryawan@gmail.com>
2022-12-02 16:19:53 +0700

    implement Email/Password sign-in

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/Home";
-import LoginScreen from "../screens/Login";
+import SignInScreen from "../screens/SignIn";
 
 const Stack = createNativeStackNavigator()
 
 export default Router = () => (
   <Stack.Navigator
-    initialRouteName="LoginScreen"
+    initialRouteName="SignInScreen"
     screenOptions={{ headerShown: false }}
   >
-    <Stack.Screen name="LoginScreen" component={LoginScreen} />
+    <Stack.Screen name="SignInScreen" component={SignInScreen} />
     <Stack.Screen name="HomeScreen" component={HomeScreen} />
   </Stack.Navigator>
 )
---
+++ src/screens/SignIn/index.js
@@ -0,0 +1,83 @@
+import auth from '@react-native-firebase/auth';
+import React, { useState } from 'react';
+import { Alert, View } from 'react-native';
+import { ActivityIndicator, Appbar, Button, TextInput, useTheme } from 'react-native-paper';
+import Spacer from '../../components/Spacer';
+
+const SignInScreen = ({ navigation }) => {
+
+  const theme = useTheme()
+
+  const [isLoading, setLoading] = useState(false)
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onSignIn = () => {
+    setLoading(true)
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        navigation.replace('HomeScreen')
+      })
+      .catch(error => {
+        let message = 'Unknown Error'
+        if (error.code === 'auth/email-already-in-use') {
+          message = 'That email address is already in use!'
+        }
+    
+        if (error.code === 'auth/invalid-email') {
+          message = 'That email address is invalid!'
+        }
+
+        setLoading(false)
+        Alert.alert(message)
+        console.error(error);
+      })
+  }
+
+  return (
+    <View
+      style={{ flex: 1 }}
+    >
+      <Appbar.Header
+        style={{ backgroundColor: theme.colors.primaryContainer }}
+      >
+        <Appbar.Content title="Sign In" />
+      </Appbar.Header>
+      <View style={{ paddingHorizontal: 24, paddingTop: 32, }}>
+        <TextInput
+          mode="outlined"
+          label="Email"
+          value={email}
+          autoCapitalize='none'
+          autoCorrect={false}
+          keyboardType='email-address'
+          onChangeText={text => setEmail(text)}
+        />
+        <Spacer height={16} />
+        <TextInput
+          mode="outlined"
+          label="Password"
+          value={password}
+          secureTextEntry
+          onChangeText={text => setPassword(text)}
+        />
+        <Spacer height={48} />
+        {isLoading
+          ? <ActivityIndicator
+            animating
+          />
+          : <Button
+            mode="contained-tonal"
+            onPress={onSignIn}
+          >
+            Sign In
+          </Button>
+        }
+      </View>
+    </View>
+  )
+}
+
+export default SignInScreen
